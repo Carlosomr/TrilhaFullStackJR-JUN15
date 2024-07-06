59e10ca2fda54f37f812ee507dfcb30e2a3128c9
Carlos Eduardo <carloseduardo.moura.contato@gmail.com>
2024-07-06 11:15:46 -0300

    criado o arquivo backend

---
+++ backend/codigocerto/src/main/java/com/gerenciamento/projeto/codigocerto/CodigocertoApplication.java
@@ -0,0 +1,13 @@
+package com.gerenciamento.projeto.codigocerto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodigocertoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodigocertoApplication.class, args);
+	}
+
+}
---
+++ backend/codigocerto/src/test/java/com/gerenciamento/projeto/codigocerto/CodigocertoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gerenciamento.projeto.codigocerto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodigocertoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
